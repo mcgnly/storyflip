54ac3cea1ed1ad7f7147d7fb6fc6ac7f3de7d81e
mcgnly <katie@mcgnly.com>
2018-12-22 22:31:03 +0100

    fix upload and return success

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -10,11 +10,12 @@ const uploadApi = app => {
         req.setTimeout(0) // no timeout
         const fileToUpload = req.file.buffer;
         const {order_id, description} = req.body;
-        fs.writeFile(`customerUploads/${order_id}.pdf`, fileToUpload, (err) => {  
+        fs.writeFile(`server/customerUploads/${order_id}.pdf`, fileToUpload, (err) => {  
             // throws an error, you could also catch it here
             if (err) throw err;
             // success case, the file was saved
             console.log('pdf saved!');
+            res.status(200).send({ success: 'file saved successfully!' });
         });
     });
 
